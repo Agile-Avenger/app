b1c699783b522065635b00c0b7fbbe5337df181a
jainankit0811 <ankitd0811@gmail.com>
2024-10-25 13:04:34 +0530

    deleted agile-frontend

--- agile_Frontend/android/app/build/intermediates/cxx/Debug/k4m2j3u4/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\ankit\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\app\\app\\agile_Frontend\\android\\app\\.cxx\\Debug\\k4m2j3u4\\arm64-v8a" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
